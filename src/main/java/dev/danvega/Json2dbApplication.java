aa070d888148650e6328de38f56a65f648155908
dfaassis-2025 <33586548+dfaassis@users.noreply.github.com>
2026-04-17 14:24:57 -0300

    new2

--- src/main/java/dev/danvega/Json2dbApplication.java
+++ src/main/java/dev/danvega/Json2dbApplication.java
@@ -2,7 +2,7 @@ package dev.danvega;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import dev.danvega.service.UserService;	
+import dev.danvega.service.UserService;
 import dev.danvega.controller.UserController;
 import dev.danvega.domain.User;
 
@@ -20,33 +20,39 @@ public class Json2dbApplication {
 
 	@SuppressWarnings("unused")
 	private final UserService userService;
-    @SuppressWarnings("unused")
+	@SuppressWarnings("unused")
 	private final UserController userController;
 
-    Json2dbApplication(UserController userController, UserService userService) {
-        this.userController = userController;
-        this.userService = userService;
-    }
+	Json2dbApplication(UserController userController, UserService userService) {
+		this.userController = userController;
+		this.userService = userService;
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(Json2dbApplication.class, args);
 	}
 
 	@SuppressWarnings("null")
 	@Bean
-	CommandLineRunner runner(UserService userService){
-	    return args -> {
-			// read JSON and load json
+	CommandLineRunner runner(UserService userService) {
+		return args -> {
 			ObjectMapper mapper = new ObjectMapper();
-			//TypeReference<List<User>>  typeReference = new TypeReference<List<User>>(){};
+			// 1. Defina que você espera uma LISTA de usuários
+			TypeReference<List<User>> typeReference = new TypeReference<List<User>>() {
+			};
 			InputStream inputStream = TypeReference.class.getResourceAsStream("/json/users.json");
+
 			try {
-				List<User> users = mapper.readValue(inputStream, new TypeReference<List<User>>(){});
-				userService.save((User) users);
-				System.out.println("Users Saved!");
-			} catch (IOException e){
-				System.out.println("Unable to save users: " + e.getMessage());
+				// O erro acontece aqui se o tipo do lado esquerdo for apenas 'User'
+				List<User> users = mapper.readValue(inputStream, typeReference);
+
+				// 2. O seu UserService deve ter um método que aceite List<User>
+				userService.save(users);
+
+				System.out.println("Usuários salvos com sucesso!");
+			} catch (IOException e) {
+				System.out.println("Erro ao ler JSON: " + e.getMessage());
 			}
-	    };
+		};
 	}
 }
